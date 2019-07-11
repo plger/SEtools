a2db745fc3a8b3ec2fc3e85e7468555c03458e61
Pierre-Luc Germain <pl.germain@gmail.com>
2019-07-11 16:30:26 +0200

    documented data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' @rdname data
+#' @name data
+#' @aliases data SE
+#'
+#' @title Example dataset
+#'
+#' @description
+#' A \code{\link[SummarizedExperiment]{SummarizedExperiment}} containing (a
+#' subset of) whole-hippocampus RNAseq of mice after different stressors.
+#'
+#' @return a \code{\link[SummarizedExperiment]{SummarizedExperiment}}.
+#'
+#' @references
+#' Floriou-Servou et al. (2018).
+#' Distinct Proteomic, Transcriptomic, and Epigenetic Stress Responses in Dorsal
+#'  and Ventral Hippocampus.
+#' \emph{Biological Psychiatry}, \bold{84}(7): 531-541.
+#' DOI: 10.1016/j.biopsych.2018.02.003.
+NULL
