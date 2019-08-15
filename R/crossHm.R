b641e8af665562fe34e329ee0e944a064a9c3652
Pierre-Luc Germain <pl.germain@gmail.com>
2019-08-15 10:59:27 +0200

    fixed missing n in colorscale

--- R/crossHm.R
+++ R/crossHm.R
@@ -112,7 +112,7 @@ crossHm <- function( ses, genes, do.scale=TRUE, uniqueColorScale=FALSE,
     srn <- show_rownames
     if(srn=="once") srn <- i==length(ses)
     if(!is.function(hmcols)){
-        tcols <- colorRamp2(.getBreaks(dats[[i]]),hmcols)
+        tcols <- colorRamp2(.getBreaks(dats[[i]], length(hmcols)+1),hmcols)
     }else{
         tcols <- hmcols
     }
